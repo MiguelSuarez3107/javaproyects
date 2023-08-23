b872644b02d1d1c42f92dcabd80901146a932cd4
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-08-23 09:41:53 -0500

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,18 @@
+package tree;
+public class Tree {
+    public static void main(String[] args) {
+        System.out.println("Hi, Im Miguel and Im learning to code in java :)");
+        System.out.println("Im going to plant a tree in this morning" + " Im ready to get my hands dirty");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******"); 
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+    }
+}
