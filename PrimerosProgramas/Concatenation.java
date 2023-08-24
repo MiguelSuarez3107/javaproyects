6a69bd4eede43e19b807ff07ffe25c70ce0099d8
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-08-24 14:39:25 -0500

    Add files via upload

---
+++ PrimerosProgramas/Concatenation.java
@@ -0,0 +1,9 @@
+package concatenation;
+public class Concatenation {
+    public static void main(String[] args) {
+    int animals = 35;
+    String species = "cats";
+    String HouseDescription = "Our house has " + animals +" "+ species +"!!";
+    System.out.println(HouseDescription);
+    }
+}
