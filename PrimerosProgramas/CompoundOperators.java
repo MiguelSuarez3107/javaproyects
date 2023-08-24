61093bbd141e70068b28d120b9c9ec456d7c98dc
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-08-24 14:34:22 -0500

    Add files via upload

---
+++ PrimerosProgramas/CompoundOperators.java
@@ -0,0 +1,12 @@
+package compoundoperators;
+public class CompoundOperators {
+    public static void main(String[] args) {
+    int AnyNumber = 17;
+    System.out.println(AnyNumber);
+    AnyNumber -= 3;
+    System.out.println(AnyNumber);
+    AnyNumber /= 2;
+    System.out.println(AnyNumber);
+    }
+    
+}
