44cb437581092805ac19353563dfd87b5e3d47a5
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 09:19:43 -0500

    Add files via upload

---
+++ PrimerosProgramas/Variable.java
@@ -0,0 +1,10 @@
+package variable;
+public class Variable {
+    public static void main(String[] args) {
+    String name = "Miguel Angel Suarez Gonzalez";
+    int year=2005;
+        System.out.println(name);
+        System.out.println(year);
+    }
+    
+}
