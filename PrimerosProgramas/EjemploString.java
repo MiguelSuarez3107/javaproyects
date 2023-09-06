ebcadc8eb311535c82f9f329e3b59ee98c2c5f5d
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 09:11:21 -0500

    Add files via upload

---
+++ PrimerosProgramas/EjemploString.java
@@ -0,0 +1,9 @@
+package ejemplostring;
+public class EjemploString {
+    public static void main(String[] args) {
+        
+    String song = "No me hubieras dejado esa noche, porque esa misma noche encontre un amor.";
+        System.out.println(song);
+    }
+    
+}
