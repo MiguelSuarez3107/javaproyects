77cb4ef83441ee9d8d26389d93b28ea9f43439a2
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 08:52:51 -0500

    Add files via upload

---
+++ PrimerosProgramas/RealicionesDeOperacion.java
@@ -0,0 +1,12 @@
+package realicionesdeoperacion;
+public class RealicionesDeOperacion {
+    public static void main(String[] args) {
+    double CreditosObtenidos=176.5;
+    double CreditosDeSeminario=8;
+    double CreditosParaGraduarse=180;
+    double CreditosDespuesDeSeminario=CreditosDeSeminario+CreditosObtenidos;
+        System.out.println(CreditosObtenidos > CreditosParaGraduarse);
+        System.out.println(CreditosParaGraduarse < CreditosDespuesDeSeminario);
+    }
+    
+}
