adf22e56ec6d8961adab4b42008295765724d4a7
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 09:37:10 -0500

    Add files via upload

---
+++ Condicionales/CondicionalEjemplo1.java
@@ -0,0 +1,13 @@
+package condicionalejemplo1;
+public class CondicionalEjemplo1 {
+    public static void main(String[] args) {
+    int x =10;
+    int y =12;
+    if(x+y > 20){
+        System.out.println("x + y es mayor que 20");
+    }
+    else{
+        System.out.println("x + y es menor que 20");
+    }
+    }
+}
