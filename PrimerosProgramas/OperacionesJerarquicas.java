f24c70911d0a1044b9238a2016185da66dc3ee32
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 08:47:46 -0500

    Add files via upload

---
+++ PrimerosProgramas/OperacionesJerarquicas.java
@@ -0,0 +1,13 @@
+package operacionesjerarquicas;
+public class OperacionesJerarquicas {
+    public static void main(String[] args) {
+    int num1=5%2-(4*2-1);
+    int num2=(3+(2*2-5))+6-5;
+    int num3=5*4%3-2+1;
+    
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+    
+}
