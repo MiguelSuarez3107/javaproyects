9d90512c12399596620055bdb3f4bd61edab7e2f
MiguelSuarez3107 <143001545+MiguelSuarez3107@users.noreply.github.com>
2023-09-06 08:57:11 -0500

    Add files via upload

---
+++ PrimerosProgramas/RelacionesDeOperaciones2.java
@@ -0,0 +1,15 @@
+package relacionesdeoperaciones2;
+public class RelacionesDeOperaciones2 {
+    public static void main(String[] args) {
+    int songsA = 9;
+    int songsB = 9;
+    int albumLengthA = 41;
+    int albumLengthB = 53;
+    boolean sameNumberOfSongs = songsA == songsB;
+    boolean differenLength = albumLengthA != albumLengthB;
+    
+        System.out.println(sameNumberOfSongs);
+        System.out.println(differenLength);
+    }
+    
+}
